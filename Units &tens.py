129b769dd70706137baf17d0d9b2f2706da517f0
Satishburavelli <138653813+Satishburavelli@users.noreply.github.com>
2024-12-22 21:38:09 +0100

    Add files via upload

---
+++ Units &tens.py
@@ -0,0 +1,9 @@
+num=int(input("Enter the number:"))
+def unit():
+   unit=int(num%1)
+   print("the unit digit number:",+unit)
+unit()
+def ten():
+    ten=int(num%10)
+    print("the ten digit number:",+ten)
+ten()
